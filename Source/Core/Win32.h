7cf6738406b0a6420b8c8535c45b4e574d2b5824
Achal Pandey <pandeyachal.ap@gmail.com>
2020-08-15 18:54:02 +0530

    Initial commit

---
+++ Source/Core/Win32.h
@@ -0,0 +1,43 @@
+#ifndef WIN32_H
+
+#define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOKERNEL
+#define NONLS
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE
+#define NOTAPE
+
+#define STRICT
+
+#include <Windows.h>
+
+#define WIN32_H
+#endif
---
+++ Source/WinMain.cpp
@@ -0,0 +1,8 @@
+#include "Core/Win32.h"
+#include "Core/Types.h"
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, PSTR command_line, INT show_code)
+{
+
+    return 0;
+}
